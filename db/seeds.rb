0185bb546bb47223735adef99c16553f9e9cfdcd
asell22 <alexanderjsell@gmail.com>
2014-11-26 12:26:16 -0800

    exercise completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+person = Person.new
+person.first_name = "Marlon"
+person.last_name = "Brando"
+person.eye_color = "Dark Brown"
+person.date_of_birth = Date.new(1924,4,3)
+person.height_in_inches = 69
+person.awesome = true
+person.save
+
+person = Person.new(first_name: "Marilyn", last_name: "Monroe", eye_color: "Blue", date_of_birth: Date.new(1926,6,1), height_in_inches: 65, awesome: true)
+person.save
+
+person = Person.create({first_name: "Robert", last_name: "Mitchum", eye_color: "Brown", date_of_birth: Date.new(1917,8,6), height_in_inches: 73, awesome: true })
+
+person = Person.create({first_name: "Tennessee", last_name: "Williams", eye_color: "Gray", date_of_birth: Date.new(1911,3,26), height_in_inches: 66, awesome: true })
+
+person = Person.create({first_name: "Adolf", last_name: "Hitler", eye_color: "Blue", date_of_birth: Date.new(1889,4,20), height_in_inches: 68, awesome: false })
+
+person = Person.create({first_name: "Joe", last_name: "Monroe", eye_color: "Green", date_of_birth: Date.new(1933,5,20), height_in_inches: 65, awesome: true })
+
+person = Person.create({first_name: "Curtis", last_name: "Monroe", eye_color: "Blue", date_of_birth: Date.new(1976,8,31), height_in_inches: 65, awesome: false })
+
+person = Person.create({first_name: "Bob", last_name: "Monroe", eye_color: "Hazel", date_of_birth: Date.new(1845,1,10), height_in_inches: 65, awesome: true })
+
+person = Person.create({first_name: "Lyle", last_name: "Monroe", eye_color: "Blue", date_of_birth: Date.new(2012,12,26), height_in_inches: 65, awesome: false })
